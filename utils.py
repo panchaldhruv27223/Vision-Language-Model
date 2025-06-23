48ad91e59b160e4bbc7397387bcef064a44537e8
panchaldhruv27223 <panchaldhruv27223@gmail.com>
2025-06-23 20:59:01 +0530

    Code By Dhruv Panchal

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def load_hf_model(model_path, device):
     for safetensors_file in safetensors_files:
         with safe_open(safetensors_file, framework="pt", device="cpu") as f:
             for key in f.keys():
-                tensors[key] = f.get_tensors(key)
+                tensors[key] = f.get_tensor(key)
 
     with open(os.path.join(model_path, "config.json"), "r") as f:
         model_config_file = json.load(f)
